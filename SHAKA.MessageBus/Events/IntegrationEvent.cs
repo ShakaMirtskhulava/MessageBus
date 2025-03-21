85d254c2170d746d2a63ba9abae778b4d7ca6bbf
Cherbaki Zondikize <samudcherbak2018@gmail.com>
2025-03-21 12:14:07 +0400

    Directly reference all packages

---
+++ SHAKA.MessageBus/Events/IntegrationEvent.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text.Json.Serialization;
+
+namespace MessageBus.Events;
+
+public record IntegrationEvent
+{
+    public IntegrationEvent()
+    {
+        Id = Guid.NewGuid();
+        CreationDate = DateTime.UtcNow;
+    }
+
+    public IntegrationEvent(object entityId)
+    {
+        EntityId = entityId;
+        Id = Guid.NewGuid();
+        CreationDate = DateTime.UtcNow;
+    }
+
+    [JsonInclude]
+    public Guid Id { get; set; }
+    [JsonInclude]
+    public DateTime CreationDate { get; set; }
+    [JsonInclude]
+    public string? CorrelationId { get; set; }
+    [JsonInclude]
+    public object? EntityId { get; set; }
+}
+
